48cc3e9c217c925e9cb7a354018fc7736c8c560e
Han Kim <hanniek00@gmail.com>
2021-08-01 18:18:13 -0500

    Create Xcode project

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Hannie Kim on 8/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
